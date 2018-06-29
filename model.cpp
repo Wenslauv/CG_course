74a3feabc3ecf993c872dd4120efc4d143951ee8
Viachaslau <wenslauv@gmail.com>
2018-06-29 18:06:01 +0300

    model functions

--- model.cpp
+++ model.cpp
@@ -3,9 +3,26 @@
 
 Model::Model(const std::string& name) {
     
-    
-    
-    
-    
-    
+    // do nothing now;
+}
+
+int Model::vertices_count() const {
+    return vertices.size();
+}
+
+int Model::faces_count() const {
+    return faces.size();
+}
+
+Vector3f Model::get_vertice(int index) const {
+    if (index > vertices.size())
+        return Vector3f();
+    return vertices.at(index);
+}
+
+std::vector<int> Model::get_face(int index) const {
+    if (index > faces.size()) {
+        return std::vector<int>();
+    }
+    return faces.at(index);
 }
--- model.h
+++ model.h
@@ -1,9 +1,12 @@
 #pragma once
 
-
 #include <vector>
 #include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
+#include "geometry.h"
 
 
 class Model
@@ -11,10 +14,13 @@ class Model
 public:
     Model(const std::string& filename);
     
+    int vertices_count() const;
+    int faces_count() const;
     
-    
+    Vector3f get_vertice(int index) const;
+    std::vector<int> get_face(int index) const;
     
 private:
-    std::vector<std::vector<int> > faces;
-    std::vector<int> vertices;
+    std::vector<Vector3f> vertices;
+    std::vector<std::vector<int>> faces;
 };
